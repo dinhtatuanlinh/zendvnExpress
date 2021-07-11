209107d9479bbc5a688790218534ed5f6ce86c0f
dinh ta tuan linh <dinhtatuanlinh@gmail.com>
2021-07-11 21:34:40 +0700

    fix asynchronous

--- helper/utility.js
+++ helper/utility.js
@@ -6,27 +6,28 @@ let statusFilter = [
 ];
 var delay = (status) => {
     return new Promise((res, rej) =>{
-        statusFilter.forEach((item, index) => {
+        for(var item of statusFilter) {
             let cond = {};
             if (item.name !== 'all') cond = {status: item.name};
             if (item.name === status) statusFilter[index].class = 'success';
             ItemsModel.count(cond).then((data)=>{
                 statusFilter[index].count = data;
             })
-        })
+        }
 
     })
     
 };
-var statusButton = (status)=>{
+var statusButton = async (status)=>{
     
     // đếm số document của collection
     
-    delay(status).then(()=>{
-        console.log(statusFilter);
-        return statusFilter;
-    });
-    
+    // delay(status).then(()=>{
+    //     console.log(statusFilter);
+    //     return statusFilter;
+    // });
+    await delay(status);
+    return statusFilter;
     
 }
 
