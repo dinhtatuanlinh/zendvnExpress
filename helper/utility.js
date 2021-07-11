168ed2409c5be522c64a805318fb2f3988e2743c
dinh ta tuan linh <dinhtatuanlinh@gmail.com>
2021-07-11 20:58:35 +0700

    fix asynchronous

--- helper/utility.js
+++ helper/utility.js
@@ -1,26 +1,28 @@
 const ItemsModel = require('./../schemas/items');
-var statusButton = (status)=>{
-    let statusFilter = [
-        {name: 'all', count: null, link: '#', class: 'default'},
-        {name: 'active', count: null, link: '#', class: 'default'},
-        {name: 'inactive', count: null, link: '#', class: 'default'},
-    ];
-    // đếm số document của collection
-    var delay = async() => {
+var delay = () => {
+    return new Promise((res, rej) =>{
         statusFilter.forEach((item, index) => {
             let cond = {};
             if (item.name !== 'all') cond = {status: item.name};
             if (item.name === status) statusFilter[index].class = 'success';
             ItemsModel.count(cond).then((data)=>{
                 statusFilter[index].count = data;
-                console.log(statusFilter[index]);
             })
         })
-    };
-    delay().then(async() => {
-        console.log(statusFilter);
-        return statusFilter;
-    });
+    })
+    
+};
+var statusButton = async (status)=>{
+    let statusFilter = [
+        {name: 'all', count: null, link: '#', class: 'default'},
+        {name: 'active', count: null, link: '#', class: 'default'},
+        {name: 'inactive', count: null, link: '#', class: 'default'},
+    ];
+    // đếm số document của collection
+    
+    await delay();
+    console.log(statusFilter);
+    return statusFilter;
     
 }
 
