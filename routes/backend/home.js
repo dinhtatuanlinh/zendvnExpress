533294fd08ec36986aaa4145a0f61d302e6fa9fb
dinh ta tuan linh <dinhtatuanlinh@gmail.com>
2021-07-11 15:12:17 +0700

    filter button

--- routes/backend/home.js
+++ routes/backend/home.js
@@ -8,11 +8,26 @@ router.get('/', (req, res, next) => {
     // ItemsModel.find({}, function (err, items) { // thay bằng phương thức then để xử lý bất đồng bộ
     //     console.log(items);
     // });
+    let statusFilter = [
+        {name: 'all', count: null, link: '#', class: 'default'},
+        {name: 'active', count: null, link: '#', class: 'success'},
+        {name: 'inactive', count: null, link: '#', class: 'warning'},
+    ];
+    // đếm số document của collection
+    statusFilter.forEach((item, index) => {
+        let cond = {};
+        if (item.name !== 'all') cond = {status: item.name};
+        ItemsModel.count(cond).then((data)=>{
+            statusFilter[index].count = data;
+        })
+    })
+    
     ItemsModel.find({}).then(( items) => { // thay bằng phương thức then để xử lý bất đồng bộ
             console.log(items);
             res.render('inc/admin/list', { 
                 title: 'abc list page',
-                items: items
+                items: items,
+                statusFilter: statusFilter
             });
         });
     
