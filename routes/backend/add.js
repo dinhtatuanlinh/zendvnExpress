1b15cbe596118da37ea84bb6168ead86e3036656
dinh ta tuan linh <dinhtatuanlinh@gmail.com>
2021-07-17 15:08:01 +0700

    add edit items

--- routes/backend/add.js
+++ routes/backend/add.js
@@ -1,13 +1,17 @@
 var express = require('express');
 var router = express.Router();
-
+const ItemsModel = require('./../../schemas/items'); // kéo module items trong schemas để truy cập bảng items trong database
+const utility = require('./../../helper/utility'); // kéo các hàm trong utility helper vào 
 /* GET users listing. */
 router.get('(/:id)?', function(req, res, next) {
     if (req.params.id === '') {
         res.render('inc/admin/add', { title: 'add page' });
     } else {
-
-        res.render('inc/admin/add', { title: 'edit page' });
+        var data = {};
+        ItemsModel.findById(req.params.id, (err, item) => {
+            data = item;
+        });
+        res.render('inc/admin/add', { title: 'edit page', data });
     }
     // '/form(/:id)?'
     // console.log('abc');
