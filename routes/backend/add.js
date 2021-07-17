5b5bd5f270fa87b9f410e6baebd5d17a6e32d9ce
dinh ta tuan linh <dinhtatuanlinh@gmail.com>
2021-07-17 14:57:18 +0700

    create database

--- routes/backend/add.js
+++ routes/backend/add.js
@@ -2,11 +2,17 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-    console.log('abc');
-    req.flash('info', 'dinh ta tuan linh');
-    res.send('test flash');
-    res.end();
-    // res.render('inc/admin/add', { title: 'add page' });
+router.get('(/:id)?', function(req, res, next) {
+    if (req.params.id === '') {
+        res.render('inc/admin/add', { title: 'add page' });
+    } else {
+        res.render('inc/admin/add', { title: 'edit page' });
+    }
+    // '/form(/:id)?'
+    // console.log('abc');
+    // req.flash('info', 'dinh ta tuan linh');
+    // res.send('test flash');
+    // res.end();
+
 });
 module.exports = router;
