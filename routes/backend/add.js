13f992348f552370ca05ce696cd1f8b932faefe8
dinh ta tuan linh <dinhtatuanlinh@gmail.com>
2021-07-17 15:35:49 +0700

    add edit items

--- routes/backend/add.js
+++ routes/backend/add.js
@@ -4,11 +4,13 @@ const ItemsModel = require('./../../schemas/items'); // kéo module items trong 
 const utility = require('./../../helper/utility'); // kéo các hàm trong utility helper vào 
 /* GET users listing. */
 router.get('(/:id)?', function(req, res, next) {
+    var data = { name: '', status: 'novalue' };
     if (req.params.id === '') {
-        res.render('inc/admin/add', { title: 'add page' });
+        res.render('inc/admin/add', { title: 'add page', data });
     } else {
         var data = {};
-        ItemsModel.findById(req.params.id, (err, data) => {
+        ItemsModel.findById(req.params.id, (err, result) => {
+            data = result;
             res.render('inc/admin/add', { title: 'edit page', data });
         });
 
@@ -19,5 +21,14 @@ router.get('(/:id)?', function(req, res, next) {
     // res.send('test flash');
     // res.end();
 
+});
+router.post('/save', (req, res, next) => {
+    console.log(req.body);
+    var data = { name: req.body.name, status: req.body.status };
+    console.log(data);
+    new ItemsModel(data).save((err, result) => {
+        if (err) console.log(err);
+        console.log(result);
+    });
 });
 module.exports = router;
