f2255ebba49b961b2ddc2514409c4f16a60048a8
dinh ta tuan linh <dinhtatuanlinh@gmail.com>
2021-07-19 17:03:21 +0700

    validation

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const flash = require('express-flash-notification');
 const session = require('express-session');
-const validator = require('express-validator');
+var validation = require('express-validator');
 
 var expressLayouts = require('express-ejs-layouts'); //module dung để tạo layout cho giao diện
 // var mongoose = require('mongoose');
@@ -59,7 +59,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(validator({
+app.use(validation({
     isNotEqual: (val1, val2) =>{
         return val1 !== val2;
     }
