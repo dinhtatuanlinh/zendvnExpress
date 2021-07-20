98ce54ff4eaacd5ac03e9a85e7d8336ba54b2348
dinh ta tuan linh <dinhtatuanlinh@gmail.com>
2021-07-20 10:48:36 +0700

    edit

--- routes/backend/add.js
+++ routes/backend/add.js
@@ -30,12 +30,11 @@ router.get('(/:id)?', function(req, res, next) {
 router.post('/save', itemsValidation.validator,  (req, respond, next) => {
     var data = { name: req.body.name, status: req.body.status };
     var validatorErr = validationResult(req).errors;// lấy ra lỗi khi validation
-    console.log(req.query.id);
     if (req.body.id) {
         if(validatorErr.length > 0){
             res.render('inc/admin/add', { title: 'edit page', data, validatorErr });
         }else{
-            ItemsModel.updateOne({_id: req.query.id}, { name: req.body.name, status: req.body.status }, (err, affected, res)=>{
+            ItemsModel.updateOne({_id: req.body.id}, data, (err, affected, res)=>{
                 console.log(res);
                 console.log(affected);
                 console.log(err);
