6fffe0a26907f8587f68d8f031251cf90e9be1ee
dinh ta tuan linh <dinhtatuanlinh@gmail.com>
2021-07-21 14:43:18 +0700

    group

--- routes/backend/backendManager.js
+++ routes/backend/backendManager.js
@@ -5,7 +5,7 @@ var router = express.Router();
 // call child router
 
 router.use('/dashboard', require('./dashboard'));
-router.use('/all', require('./home'));
+// router.use('/all', require('./home'));
 // items
 router.use('/', require('./items/home'));
 router.use('/add', require('./items/add'));
