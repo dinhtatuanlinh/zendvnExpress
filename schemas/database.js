f510cc14bc5e6842aec87445c80fb241415802bf
dinh ta tuan linh <dinhtatuanlinh@gmail.com>
2021-07-22 15:10:13 +0700

    add users and roles

--- schemas/database.js
+++ schemas/database.js
@@ -1,10 +1,10 @@
 const mongoose = require("mongoose");
 var connect = () => {
     mongoose.connect('mongodb://tuanlinh:164342816@115.79.35.159:27017/admin');
-    const db = mongoose.connection;
-    db.on('error', () => { console.log('connection error') }); // phải dùng function hoặc arrow function ở đây
-    db.once('open', function() {
-        db.listCollections({})
+    const connectionDatabase = mongoose.connection;
+    connectionDatabase.on('error', () => { console.log('connection error') }); // phải dùng function hoặc arrow function ở đây
+    connectionDatabase.once('open', function() {
+        connectionDatabase.db.listCollections({})
             .next(function(err, collinfo) {
                 if (collinfo) {
                     console.log(collinfo);
