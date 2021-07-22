5a3f7e5750d31d78bda6590b8d17121cc359c36f
dinh ta tuan linh <dinhtatuanlinh@gmail.com>
2021-07-22 15:24:35 +0700

    connection

--- schemas/database.js
+++ schemas/database.js
@@ -3,10 +3,7 @@ const mongoose = require("mongoose");
 mongoose.connect('mongodb://tuanlinh:164342816@115.79.35.159:27017/admin');
 const connectionDatabase = mongoose.connection;
 connectionDatabase.on('error', () => { console.log('connection error') }); // phải dùng function hoặc arrow function ở đây
-connectionDatabase.once('open', function() {
-    // we're connected! 
-    console.log('database connected');
-});
+
 var itemsSchema = new mongoose.Schema({
     name: String,
     status: String,
@@ -22,12 +19,17 @@ var usersSchema = new mongoose.Schema({
     ordering: Number,
     content: String
 });
-connectionDatabase.db.listCollections({})
+connectionDatabase.once('open', function() {
+    connectionDatabase.db.listCollections({})
         .next(function(err, collinfo) {
             if (collinfo) {
                 console.log(collinfo);
             }
         });
+    // we're connected! 
+    console.log('database connected');
+});
+
 module.exports = {
     itemsModel: mongoose.model('items', itemsSchema),
     rolesModel: mongoose.model('roles', rolesSchema),
