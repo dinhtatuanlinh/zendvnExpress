f3ff3b9d863fe5cfe681bfbab8bae738e1e3a8e7
dinh ta tuan linh <dinhtatuanlinh@gmail.com>
2021-07-22 15:04:00 +0700

    add users and roles

--- schemas/roles.js
+++ schemas/roles.js
@@ -3,6 +3,12 @@ const mongoose = require("mongoose");
 const database = require('./database');
 database.connect();
 // định nghĩa các collections trong database
+database.db.listCollections({})
+    .next(function(err, collinfo) {
+        if (collinfo) {
+            console.log(collinfo);
+        }
+    });
 var schema = new mongoose.Schema({
     role: String,
 });
