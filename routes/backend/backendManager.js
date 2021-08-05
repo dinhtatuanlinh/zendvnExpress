44d1248ffdd15ede67b01a8b42513af55131ea39
dinh ta tuan linh <dinhtatuanlinh@gmail.com>
2021-08-05 16:45:12 +0700

    edit dashboard

--- routes/backend/backendManager.js
+++ routes/backend/backendManager.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 // call child router
 
-router.use('/dashboard', require('./dashboard'));
+router.use('/', require('./dashboard'));
 // router.use('/all', require('./home'));
 // items
 router.use('/items/', require('./items'));
