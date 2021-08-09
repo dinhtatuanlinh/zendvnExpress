76dbdfad04b7c4eaadcb6d79a13b5537fba030fb
dinh ta tuan linh <dinhtatuanlinh@gmail.com>
2021-08-09 17:04:15 +0700

    login

--- routes/backend/auth.js
+++ routes/backend/auth.js
@@ -6,7 +6,7 @@ var passport = require("passport"),
 var baselink = __admin + "/" + "admin";
 router.get("/", (req, res, next) => {
   // khi truyền dữ liệu qua đường dẫn để lấy được dữ liệu đấy ta thêm /:status vào router. Nếu ko có dữ liệu truyền trên đường dẫn thì thêm (/:status)? nghĩa là có ý nghĩa là chuỗi được gửi lên có cũng được ko có cũng được
-  //   console.log(__admin);
+    console.log(baselink);
   res.render(`inc/admin/auth`, {
     layout: "auth",
     baselink,
