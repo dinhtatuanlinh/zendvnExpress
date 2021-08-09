30a1c9f32e4a1cfc9142c718325545633d52f6e9
dinh ta tuan linh <dinhtatuanlinh@gmail.com>
2021-08-09 15:08:51 +0700

    login

--- routes/backend/auth.js
+++ routes/backend/auth.js
@@ -5,7 +5,7 @@ router.get("/", (req, res, next) => {
   // khi truyền dữ liệu qua đường dẫn để lấy được dữ liệu đấy ta thêm /:status vào router. Nếu ko có dữ liệu truyền trên đường dẫn thì thêm (/:status)? nghĩa là có ý nghĩa là chuỗi được gửi lên có cũng được ko có cũng được
 
   res.render(`inc/admin/auth`, {
-    layout: "admin/auth",
+    layout: 'auth',
   });
 });
 module.exports = router;
