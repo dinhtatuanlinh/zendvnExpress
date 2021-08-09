16ba810b1429d205363b842067ffc1d19f88c724
dinh ta tuan linh <dinhtatuanlinh@gmail.com>
2021-08-09 15:14:30 +0700

    login

--- routes/backend/auth.js
+++ routes/backend/auth.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 router.get("/", (req, res, next) => {
   // khi truyền dữ liệu qua đường dẫn để lấy được dữ liệu đấy ta thêm /:status vào router. Nếu ko có dữ liệu truyền trên đường dẫn thì thêm (/:status)? nghĩa là có ý nghĩa là chuỗi được gửi lên có cũng được ko có cũng được
-
+    console.log(__admin);
   res.render(`inc/admin/auth`, {
     layout: 'auth',
   });
