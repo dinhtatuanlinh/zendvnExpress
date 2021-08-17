5f7e1b4295a358e7d29a4967ceb12b830f547247
dinh ta tuan linh <dinhtatuanlinh@gmail.com>
2021-08-17 14:55:10 +0700

    login

--- routes/backend/backendManager.js
+++ routes/backend/backendManager.js
@@ -9,7 +9,7 @@ router.use('/', (req, res, next) => {
     if (req.isAuthenticated()) { // isAuthenticated để xác định đã được login rồi hay chưa rồi sẽ trả về true chưa trả về false
         next();
     } else {
-        res.redirect(`/${__admin}`);
+        res.redirect(`/${__admin}/admin`);
     }
 }, require('./dashboard'));
 // router.use('/all', require('./home'));
